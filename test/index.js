aca632ee0558c15a504a2ab7b71a60c75ff087e1
Thiago Lagden <lagden@gmail.com>
2019-08-14 21:35:05 -0300

    update devDeps and add more tests

--- test/index.js
+++ test/index.js
@@ -4,6 +4,24 @@ import test from 'ava'
 import MysqlPool from '..'
 
 const pool = new MysqlPool()
+const dbName = 'test'
+const table = 'tmp_tbl'
+
+test.before(async () => {
+	const createTable = [
+		'CREATE DATABASE IF NOT EXISTS ??;',
+		'CREATE TABLE IF NOT EXISTS ?? (',
+		'`id` int(11) unsigned NOT NULL AUTO_INCREMENT,',
+		'`name` varchar(30),',
+		'`age` int(11),',
+		'PRIMARY KEY (`id`));'
+	].join('\n')
+	await pool.query(createTable, [dbName, `${dbName}.${table}`])
+})
+
+test.after(async () => {
+	await pool.query('DROP DATABASE ??', [dbName])
+})
 
 test('ok', async t => {
 	const {results: [{total}]} = await pool.query('SELECT 1 + ? as total', [1])
@@ -13,4 +31,24 @@ test('ok', async t => {
 test('err', async t => {
 	const error = await t.throwsAsync(pool.query('SELEC 1 + 1 as total'))
 	t.is(error.message.split(':')[0], 'ER_PARSE_ERROR')
+})
+
+test('bulk', async t => {
+	const data = [{name: 'Sabrina', age: 40}, {name: 'Lucas', age: 6}, {name: 'Thiago', age: 37}]
+	const keys = Object.keys(data[0])
+	const items = []
+	for (const o of data) {
+		const item = []
+		for (const k of keys) {
+			item.push(o[k])
+		}
+
+		items.push(item)
+	}
+
+	const {results: {insertId}} = await pool.query(`INSERT INTO ?? (${keys.join(', ')}) VALUES ?`, [`${dbName}.${table}`, items])
+	const {results} = await pool.query(`SELECT ${keys.join(', ')} FROM ??`, [`${dbName}.${table}`])
+	t.is(insertId, 1)
+	t.is(results.shift().name, 'Sabrina')
+	t.is(results.pop().name, 'Thiago')
 })
